d91eb8d037fba215aad8e50b95047cf38e5de043
Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>
2023-12-04 20:48:10 +0530

    feat(tiptap): add tiptap editor
    Signed-off-by: Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>

---
+++ src/ui/AdminTaskSubmit.jsx
@@ -0,0 +1,13 @@
+import Tiptap from './helpers/editor.jsx';
+
+const AdminTaskSubmit = () => {
+  return (
+    <div className="admin-task-submit">
+      <div className="editor-wrapper">
+        <Tiptap />
+      </div>
+    </div>
+  );
+};
+
+export default AdminTaskSubmit;
