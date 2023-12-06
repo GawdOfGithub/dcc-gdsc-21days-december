1d7cbd8cbdd4879cdc6f0c7fe6967bd2214cab06
Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>
2023-12-06 13:33:31 +0530

    chore(prod): fix prod url
    Signed-off-by: Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,19 +2,17 @@ import axios from 'axios';
 //https://node.arnabdaz.me
 
 const instance = axios.create({
-  baseURL: 'https://dcc21daysdev-production.up.railway.app/',
+  baseURL: 'https://node.arnabdaz.me',
 });
 
-
 instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token'); 
+  const token = localStorage.getItem('token');
   if (token) {
     config.headers.token = token;
   }
   if (config.method === 'post') {
     config.headers['Content-Type'] = 'application/json';
   }
-
 
   return config;
 });
